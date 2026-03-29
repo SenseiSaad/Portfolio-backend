e8e14c5f3d155fd0e6be1a921a4e3a7bf1bda6c0
Saad Waseem <obsks23@gmail.com>
2026-03-29 18:28:51 +0500

    fix: Rewrite blogs/views.py to use Blog model and correct view names

--- blogs/views.py
+++ blogs/views.py
@@ -1,50 +1,55 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import Contact
-from .serializers import ContactSerializer
+from .models import Blog
+from .serializers import BlogSerializer
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-def oneBlog(request, pk):
+def view_blog(request, pk):
     try:
-        contact = Contact.objects.get(id=pk)
-    except Contact.DoesNotExist:
+        blog = Blog.objects.get(id=pk)
+    except Blog.DoesNotExist:
         return Response({'error': 'Blog not found'}, status=404)
-    serializer = ContactSerializer(contact)
+    serializer = BlogSerializer(blog)
     return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-def allBlogs(request):
-    contacts = Contact.objects.all()
-    serializer = ContactSerializer(contacts, many=True)
+def view_all_blogs(request):
+    blogs = Blog.objects.all()
+    serializer = BlogSerializer(blogs, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-def createBlog(request):
-    serializers=ContactSerializer(data=request.data)
-    if serializers.is_valid():
-        serializers.save()
-        return Response(serializers.data)
-    return Response(serializers.errors)
+def create_blog(request):
+    serializer = BlogSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-
-def updateBlog(request, pk):
-    contact = Contact.objects.get(id=pk)
-    serializers = ContactSerializer(contact, data=request.data)
-    if serializers.is_valid():
-        serializers.save()
-        return Response(serializers.data)
-    return Response(serializers.errors)
+def update_blog(request, pk):
+    try:
+        blog = Blog.objects.get(id=pk)
+    except Blog.DoesNotExist:
+         return Response({'error': 'Blog not found'}, status=404)
+         
+    serializer = BlogSerializer(blog, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticatedOrReadOnly])
-def deleteBlog(request, pk):
-    contact = Contact.objects.get(id=pk)
-    contact.delete()
-    return Response({'message': 'Contact was deleted'})
-
+def delete_blog(request, pk):
+    try:
+        blog = Blog.objects.get(id=pk)
+    except Blog.DoesNotExist:
+        return Response({'error': 'Blog not found'}, status=404)
+    blog.delete()
+    return Response({'message': 'Blog was deleted'})
