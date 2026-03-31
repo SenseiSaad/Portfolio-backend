b6c73a57f19b3b2f305d87bba694950666737286
Saad Waseem <obsks23@gmail.com>
2026-03-31 17:31:34 +0500

    Remove created_at field from Blog model and add migration

--- blogs/models.py
+++ blogs/models.py
@@ -4,7 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
