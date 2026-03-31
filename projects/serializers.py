295f5e70f14f5e40cfcc334cddbe8383316db5f9
Saad Waseem <obsks23@gmail.com>
2026-03-31 22:17:49 +0500

    ads

--- projects/serializers.py
+++ projects/serializers.py
@@ -4,5 +4,5 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model=Project
-        fields=['id', 'category', 'title','description','tech_stack','github_link','live_link','image','created_at']
+        fields=['id', 'category', 'title','description','tech_stack','github_link','live_link','image']
         
