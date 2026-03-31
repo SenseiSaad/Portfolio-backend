75e60c239db2cd10cc047ff07da2decbfaf264ab
Saad Waseem <obsks23@gmail.com>
2026-03-31 14:36:23 +0500

    add stop script for codedeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/Portfolio-backend
-sudo docker compose down
-sudo docker compose up -d --build
+sudo docker compose down || true
 
-
-
-mkdir -p scripts
-# create both files with above content
-chmod +x scripts/deploy.sh
-git add .
-git commit -m "add codedeploy appspec"
-git push origin main
