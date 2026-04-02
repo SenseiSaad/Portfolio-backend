c222c1de09481b5828d8ade0e6561fd59cd48825
Saad Waseem <obsks23@gmail.com>
2026-04-02 19:57:35 +0500

    Fix CharField missing max_length in initial migration causing sqlite syntax error on Django 5.0

--- projects/migrations/0001_initial.py
+++ projects/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('description', models.TextField()),
-                ('tech_stack', models.CharField()),
+                ('tech_stack', models.CharField(max_length=500)),
                 ('github_link', models.URLField()),
                 ('live_link', models.URLField()),
                 ('image_url', models.URLField()),
